c9ec8064f232f249c994c526e868f7dad3f3132b
AbdullRafay <156018415+AbdullRafay@users.noreply.github.com>
2024-05-19 19:10:53 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+// chap 1 to 5
+// var myName = 'abdul rafay amir'
+// alert(myName);
+
+// var email = 'my email adress is callmeabdulrafay@gmail.com'
+// alert(email);
+ 
+
+// var myAge = '18'
+// alert(myAge);
+
+var value1 = 7
+var value2 = 15
+alert(value1 + value2);
+
+var value1 = '7'
+var value2 = '15'
+alert(value1 + value2);
+
+var value1 = 7
+var value2 = 15
+alert(value1 * value2);
